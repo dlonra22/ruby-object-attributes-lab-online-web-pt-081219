b22f8aa128f9da96c69878a3affbfbd6fb25fdb6
arnie b <arnold.bwaila@gmail.com>
2019-08-28 14:39:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,4 +2,10 @@ class Person
   def initialize(person_name)
       @this_persons_name = person_name
     end
-  def name=
+  def name= (person_name)
+      @this_persons_name = person_name
+    end
+  def name
+      @this_persons_name
+    end
+  end
