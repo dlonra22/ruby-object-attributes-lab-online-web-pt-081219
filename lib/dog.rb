523141de1f415f00b2695c935392f0f4793d9e14
arnie b <arnold.bwaila@gmail.com>
2019-08-28 14:33:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,10 @@
 class Dog 
-  def initialize(name)
-      @name = name
+  def initialize(dog_name)
+      @this_dogs_name = dog_name
     end
   def name= (dog_name)
-      this_dogs_name = dog_name
+      @this_dogs_name = dog_name
+    end
+  def name 
+      @this_dogs_name
+    end
