c80589b2967952cd5e144cebdfcd2794dcf68d16
arnie b <arnold.bwaila@gmail.com>
2019-08-28 14:50:31 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,10 +4,10 @@ class Person
     #  @this_persons_job = person_job
     #end
   def name= (person_name)
-      @this_persons_name = person_name
+      @name = person_name
     end
   def name
-      @this_persons_name
+      @name
     end
   def job= (person_job)
       @this_persons_job = person_job
